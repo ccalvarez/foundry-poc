918b349cdf70f54ffee874b3e4f2794d9526e0f1
Carolina Cordero <ccalvarez@gmail.com>
2024-10-09 13:35:37 -0600

    feat: agrega y hace deploy de DimSumVault.sol en el nodo local

--- packages/foundry/script/DeployYourContract.s.sol
+++ packages/foundry/script/DeployYourContract.s.sol
@@ -1,13 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/YourContract.sol";
+import "../contracts/DimSumVault.sol";
+import "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import "./DeployHelpers.s.sol";
+
+// contract MockERC20 is ERC20 {
+//     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+//         _mint(msg.sender, initialSupply);
+//     }
+// }
 
 contract DeployYourContract is ScaffoldETHDeploy {
   // use `deployer` from `ScaffoldETHDeploy`
   function run() external ScaffoldEthDeployerRunner {
-    YourContract yourContract = new YourContract(deployer);
+    // Deploy ERC20Mock
+    ERC20Mock mockToken = new ERC20Mock();
+    // Deploy DimSumVault with the mock token
+    DimSumVault yourContract = new DimSumVault(mockToken);
+    // YourContract yourContract = new YourContract(msg.sender);
+
     console.logString(
       string.concat(
         "YourContract deployed at: ", vm.toString(address(yourContract))
